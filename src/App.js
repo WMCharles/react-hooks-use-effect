195d49a49de1b470ece51a78dc910dcf11651ab3
WMCharles <charles.wafula@student.moringaschool.com>
2022-09-23 09:25:27 +0300

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React, { useState, useEffect } from "react";
+import {useEffect, useState} from "react"
+import DogPics from "./DogPics"
 
-function App() {
-  useEffect(() => {
-    console.log("useEffect called");
-  });
+function Effect(){
 
-  console.log("Component rendering");
+    const [count, setCount] = useState(0)
+    const [text, setText] = useState("")
 
-  return <button>Click Me</button>;
+    useEffect(
+        () => {document.title = text},
+        [text]
+    )
+
+    useEffect (() => {
+        setTimeout(() => setCount(0), 5000)
+    }, [])
+
+    useEffect(
+        () => console.log("useEffect called!"),
+        [count]
+    )
+
+    console.log("Component Rendering !!")
+
+    return (
+        <div>
+          <DogPics/>
+            <button onClick={() => setCount((count) => count + 1)}>Clicked {count} times</button>
+            <input
+                type="text"
+                placeholder="Type away..."
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
+        </div>
+    )
 }
 
-export default App;
+export default Effect;
